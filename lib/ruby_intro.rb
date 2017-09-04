519223f53587ce022c2bce057af14cd8281dcd6e
Stephen Goeddel <sgoeddel247@gmail.com>
2017-09-04 02:40:00 +0000

    Start to the hw.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,25 +3,52 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  total = 0
+  arr.each {|x|
+    total += x
+  }
+  
+  return total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  total = 0
+  arr.sort!
+  
+  if arr.size > 0 then
+    total += arr[arr.size - 1]
+  end
+  if arr.size > 1 then
+    total += arr[arr.size - 2]
+  end
+  
+  return total
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.size == 1 then
+    return false
+  end
+
+  arr.each_with_index { |i, ind|
+      rest = arr[(ind + 1)..-1]
+      rest.each { |j|
+        if (i + j) == n then
+          return true
+        end
+      }
+  }
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s =~ /^[^aeiou\W]/i
 end
 
 def binary_multiple_of_4? s
